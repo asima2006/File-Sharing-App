bd3d9c272a6a3996411b381c6f661b11d55b3072
Mohit Singh Raghav <118093505+asima2006@users.noreply.github.com>
2024-06-24 15:45:49 +0530

    updating backend uri

--- frontend/src/FileUpload.jsx
+++ frontend/src/FileUpload.jsx
@@ -17,7 +17,7 @@ const FileUpload = () => {
     formData.append('file', file);
 
     try {
-      const res = await axios.post('http://localhost:5002/upload', formData, {
+      const res = await axios.post(`${process.env.REACT_APP_API_URL}/upload`, formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
